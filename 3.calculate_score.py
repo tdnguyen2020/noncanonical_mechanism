66b306f87747cee20dbf3b46301e14e96c4c3d1c
unknown <nguyentrungduc154@gmail.com>
2022-10-30 21:39:56 +0800

    update code files

--- 3.calculate_score.py
+++ 3.calculate_score.py
@@ -12,62 +12,6 @@ import pandas as pd
 import numpy as np
 import using_now as us
 
-#%% (DONE) PROCESS SUM COUNT
-
-def import_prdt(path, sample_name):
-    df_refseq = us.import_refseq()
-    
-    # sample_name = 'd3g1lg4_f3'
-    df_prdt =  pd.read_csv(f'./{path}/{sample_name}_var_bar_count_uniq.txt', 
-                           sep = '\t', names =['var', 'read_seq', 'count'])
-    
-    # df_prdt = test.head(1000000)
-
-    print(f'Start {sample_name}:' , df_prdt['count'].sum())  
-    df_prdt = df_prdt[df_prdt['read_seq'].str.len() >= 15]
-    df_prdt = df_prdt[df_prdt['read_seq'].str.len() <= 54]
-
-    df_prdt['ref_seq'] = df_prdt.apply(lambda x: df_refseq['ref_seq'][x['var']], axis =1)
-        
-    df_prdt['cleavage_site'] = df_prdt.apply(lambda x: 
-        x['ref_seq'][54:].find(x['read_seq'][:8]), axis =1)  
-
-    df_prdt = df_prdt[df_prdt['cleavage_site'] != -1]
-    print('After map:', df_prdt.groupby(['cleavage_site'])['count'].sum().sum()) 
-
-    df_prdt['cleavage_site'] = 86 - df_prdt['cleavage_site'] - 54
-    df_prdt = df_prdt[df_prdt['cleavage_site'].isin([i for i in range(-5,6)])]
-    print('After take -5 to 5:', df_prdt['count'].sum())
-    
-    df_prdt = df_prdt.groupby(['var', 'cleavage_site'])['count'].sum().reset_index()
-    df_prdt = df_prdt.pivot(index = 'var', columns = 'cleavage_site', values = 'count')
-    
-    return(df_prdt)
-
-def import_ctrl(path):
-    df_ctrl = pd.read_csv(f'./{path}/ctrl_sumcount.txt', 
-                          sep ='\t', names = ['var', 'barcode_number', 'count'] )
-    df_ctrl = pd.DataFrame(df_ctrl.groupby(['var'])['count'].sum())
-    df_ctrl.columns = ['ctrl']
-    return(df_ctrl)
-
-def sum_rawcount():
-    
-    # path = 'set1'
-    path = 'set2'
-    
-    df_ctrl = import_ctrl(path)
-    df_d3g1lg4  = import_prdt(path, 'd3g1lg4') # 35950702 --> 23724948
-    df_d3g2l  = import_prdt(path, 'd3g2l') # 22848952 --> 14463736
-
-    df_ctrl['d3g1lg4'] = df_d3g1lg4.sum(axis = 1)
-    df_ctrl['d3g2l'] = df_d3g2l.sum(axis = 1)
-    df_ctrl.fillna(0).astype(int).to_csv(f'{path}/rawcount.txt', sep = '\t',
-                                         index = True, header = True)
-
-    df_d3g1lg4.to_csv(f'raw_count/{path}/d3g1lg4_rawcount.txt', sep = '\t', index = True, header = True)
-    df_d3g2l.to_csv(f'raw_count/{path}/d3g2l_rawcount.txt', sep = '\t', index = True, header = True)
-
 
 #%% (DONE) CALCULATE GCE (GLOBAL CLEAVAGE EFFICIENCY)
 
